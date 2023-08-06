de3843cf985d34e6ccdbc949c362338eff9144da
Hugo Damois <hugo.damois@efrei.net>
2023-08-06 11:40:04 +0200

    updated bot status

--- src/events/ready/console-log.js
+++ src/events/ready/console-log.js
@@ -4,6 +4,7 @@ module.exports = (client) => {
     console.log(`${client.user.tag} is ready, shitsorm started`); 
     client.user.setActivity({
         name: 'Crotte sa niche',
-        type: ActivityType.Custom,
+        type: ActivityType.Streaming,
+        url: 'https://www.youtube.com/shorts/C0qRymKS3Bw',
     });
 };
